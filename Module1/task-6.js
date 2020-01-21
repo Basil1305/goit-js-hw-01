28538d49396eab982979f75b2317dd11804f54ff
Basil <52466886+Basil1305@users.noreply.github.com>
2020-01-21 12:25:02 +0200

    Create task-6.js

---
+++ Module1/task-6.js
@@ -0,0 +1,9 @@
+let input;
+let userTotal = 0;
+
+do {
+   input = prompt(`Введите число`);
+   userTotal = userTotal + Number(input);
+ } while (input !== null);
+
+ alert(`Общая сумма чисел равна ${userTotal}`);
