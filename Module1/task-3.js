6e3d7cc37bd678c73196361e4fb5fcd5fa64193a
Basil <52466886+Basil1305@users.noreply.github.com>
2020-01-21 12:27:30 +0200

    Update task-3.js

--- Module1/task-3.js
+++ Module1/task-3.js
@@ -6,5 +6,5 @@ let message = prompt("Введите пароль администратора")
 if (message === ADMIN_PASSWORD) {
   console.log("Добро пожаловать!");
 } else if (message === null) {
-  console.log("Отменено пользователем!");
-} else console.log("Доступ запрещен, неверный пароль!");
+  console.log("Отменено пользователем !");
+} else console.log("Доступ запрещен, неверный пароль !");
