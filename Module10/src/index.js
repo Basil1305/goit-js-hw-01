11c065f8652c7d8c07d758ac3a2f833128c19565
Basil Koval <lodupu8989@g,gmail.com>
2020-03-30 23:48:56 +0300

    Updt

---
+++ Module10/src/index.js
@@ -0,0 +1,39 @@
+import './styles.css';
+import menuHbs from './menuH.hbs';
+import menu from './menu.json';
+
+const switchTheme = document.querySelector('.js-switch-input');
+const switchThemeBody = document.querySelector('body');
+const Js_menu = document.querySelector('.js-menu');
+
+switchTheme.addEventListener('change', chooseTheme);
+if (localStorage.getItem('theme') !== 'light') {
+  SetDark();
+  switchTheme.setAttribute('checked', 'checked');
+} else {
+  SetLight();
+}
+function chooseTheme() {
+  if (switchThemeBody.classList.contains('dark-theme')) {
+    SetLight();
+  } else {
+    SetDark();
+  }
+}
+
+function SetLight() {
+  switchThemeBody.classList.remove('dark-theme');
+  switchThemeBody.classList.add('light-theme');
+  localStorage.setItem('theme', 'light');
+}
+function SetDark() {
+  switchThemeBody.classList.remove('light-theme');
+  switchThemeBody.classList.add('dark-theme');
+  localStorage.setItem('theme', 'dark');
+}
+
+BuildMenu(menu);
+function BuildMenu(elements) {
+  const markup = elements.map(e => menuHbs(e)).join(' ');
+  Js_menu.insertAdjacentHTML('beforeend', markup);
+}
